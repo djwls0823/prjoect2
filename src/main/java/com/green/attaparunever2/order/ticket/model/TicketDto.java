ace7617bb9d3ebf9ab76426864559fc532f45342
KwonHyeji <kwonhyeji007@gmail.com>
2025-01-22 17:50:01 +0900

    Dto 작업 후 쿼리 만드는 중

---
+++ src/main/java/com/green/attaparunever2/order/ticket/model/TicketDto.java
@@ -0,0 +1,29 @@
+package com.green.attaparunever2.order.ticket.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class TicketDto {
+    @Schema(title = "식권 PK")
+    private long ticketId;
+    @Schema(title = "주문 PK")
+    private long orderId;
+    @Schema(title = "만료시간")
+    private LocalDateTime expiredDate;
+    @Schema(title = "식권 사용 여부", description = "0: 미사용, 1: 사용")
+    private int ticketStatus;
+    @Schema(title = "식권 사용 일자")
+    private LocalDateTime useDate;
+    @Schema(title = "식권 생성일")
+    private LocalDateTime createdAt;
+    @Schema(title = "식권 금액")
+    private int point;
+}
+
