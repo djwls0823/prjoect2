3d591e43f2f0d9b7dd7144cfecc02cad01b064ee
KwonHyeji <kwonhyeji007@gmail.com>
2025-01-23 19:28:00 +0900

    사용자 결제 내역 조회 작업 중

---
+++ src/main/java/com/green/attaparunever2/order/ticket/model/PaymentUserGetRes.java
@@ -0,0 +1,11 @@
+package com.green.attaparunever2.order.ticket.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(title = "사용자 결제 내역 조회 응답")
+public class PaymentUserGetRes {
+}
