6f7c04d56fd6a97e66c7d153452c57d55bbb00c1
SugiSagong <jumoney1012@gmail.com>
2025-01-31 16:33:23 +0900

    FIX: 회사 사업자 번호 API적용(김일지), 웹소켓 STOMP 적용(사공수기)

--- src/main/java/com/green/attaparunever2/order/OrderService.java
+++ src/main/java/com/green/attaparunever2/order/OrderService.java
@@ -5,6 +5,7 @@ import com.green.attaparunever2.order.ticket.TicketMapper;
 import com.green.attaparunever2.order.ticket.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.jdbc.DataSourceUnwrapper;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class OrderService {
     private final OrderMapper mapper;
     private final TicketMapper ticketMapper;
     private final TicketService ticketService;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public long postOrder(OrderPostReq p) {
         mapper.postOrder(p);
@@ -25,6 +27,12 @@ public class OrderService {
     }
 
     public long updOrderAccess(OrderAccessPatchReq p) {
+        // 사용자에게 예약결과 알림 설정
+        messagingTemplate.convertAndSend(
+                "/queue/reservation/" + p.getOrderId() + "/user/reservation",
+                p
+        );
+
         return mapper.updOrderAccess(p);
     }
 
